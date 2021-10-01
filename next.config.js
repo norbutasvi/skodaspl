472ef251d0013cc42f18f94a4da9131a465001e6
norbutasvi <norbutasvilius@gmail.com>
2021-10-01 15:01:22 +0300

    test

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = {
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'lt-LT',
+    localeDetection: false
     // This is a list of locale domains and the default locale they
     // should handle (these are only required when setting up domain routing)
     // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
